2521dccd7ca71881a6dfb9d400a5a0327cf4414e
Mohammed Yaseen <mohammedysn15@gmail.com>
2023-08-30 22:25:07 +0100

    Create first pairing prototype

---
+++ main.py
@@ -0,0 +1,46 @@
+import csv
+
+max_students = 5
+
+# Read mentors and add student count
+mentors = {}
+with open("data/mentors.csv") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        mentor_id, mentor_career = row
+        mentors[mentor_id] = {"career": mentor_career, "count": 0}
+
+# Read students
+students = []
+with open("data/students.csv") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        student_id, career_interest = row
+        students.append({"id": student_id, "interest": career_interest})
+
+# Pair students and mentors
+pairings = []
+unassigned_students = []
+for student in students:
+    assigned = False
+    for mentor_id, mentor in mentors.items():
+        if mentor["count"] < max_students and mentor["career"] == student["interest"]:
+            pairings.append([student["id"], mentor_id])
+            mentors[mentor_id]["count"] += 1
+            assigned = True
+            break
+    if not assigned:
+        unassigned_students.append(student)
+
+# Handle unassigned students
+for student in unassigned_students:
+    for mentor_id, mentor in mentors.items():
+        if mentor["count"] < max_students:
+            pairings.append([student["id"], mentor_id])
+            mentors[mentor_id]["count"] += 1
+            break
+
+# Output pairings
+with open("data/pairings.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(pairings)
