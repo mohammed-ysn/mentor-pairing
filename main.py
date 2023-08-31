868cde74da2a853e497ad948b7fa232aa3fc8711
Mohammed Yaseen <mohammedysn15@gmail.com>
2023-08-31 21:33:03 +0100

    Add mypy back

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from queue import PriorityQueue
 MAX_STUDENTS = 5
 
 # Read mentors and add to priority queues
-mentors = {}
-mentor_queues = {}
+mentors: dict[str, dict[str, int | str]] = {}
+mentor_queues: dict[str, PriorityQueue[tuple[int, str]]] = {}
+
 
 with open("data/mentors.csv") as f:
     reader = csv.reader(f)
     for row in reader:
+        mentor_id: str
+        mentor_career: str
         mentor_id, mentor_career = row
+
         mentors[mentor_id] = {"career": mentor_career, "count": 0}
 
         if mentor_career not in mentor_queues:
@@ -19,32 +23,35 @@ with open("data/mentors.csv") as f:
         mentor_queues[mentor_career].put((0, mentor_id))
 
 # Read students
-students = []
+students: list[dict[str, str]] = []
 
 with open("data/students.csv") as f:
     reader = csv.reader(f)
     for row in reader:
+        student_id: str
+        career_interest: str
         student_id, career_interest = row
+
         students.append({"id": student_id, "interest": career_interest})
 
 # Pair students and mentors
-pairings = []
-unassigned_students = []
+pairings: list[list[str]] = []
+unassigned_students: list[dict[str, str]] = []
 
 for student in students:
     assigned = False
 
     if student["interest"] in mentor_queues:
-        min_mentor = mentor_queues[student["interest"]].get()[1]
+        min_mentor: str = mentor_queues[student["interest"]].get()[1]
 
-        if mentors[min_mentor]["count"] < MAX_STUDENTS:
+        count = int(mentors[min_mentor]["count"])
+        if count < MAX_STUDENTS:
             pairings.append([student["id"], min_mentor, student["interest"]])
-            mentors[min_mentor]["count"] += 1
+            updated_count = count + 1
+            mentors[min_mentor]["count"] = updated_count
             assigned = True
 
-        mentor_queues[student["interest"]].put(
-            (mentors[min_mentor]["count"], min_mentor)
-        )
+            mentor_queues[student["interest"]].put((updated_count, min_mentor))
 
     if not assigned:
         unassigned_students.append(student)
